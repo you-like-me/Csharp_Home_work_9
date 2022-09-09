a37b387d83390a3039ebd797cbb6c7b4fc79b971
katerina <you-like-me@yandex.ru>
2022-09-09 12:57:54 +0300

    Add Task 064

---
+++ Task_064/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите максимальное значение интервала N ");
+int N = int.Parse(Console.ReadLine());
+void RecursionNumbers (int i)
+{
+    if (i == 1)
+    {
+        Console.Write(i + ", ");
+    }
+    else
+    {
+        Console.Write(i + ", ");
+        i--;
+        RecursionNumbers(i);
+    }
+}
+
+RecursionNumbers(N);
